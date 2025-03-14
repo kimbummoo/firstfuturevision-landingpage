0ead29073a0808ebc5687bec2226af0674e03b90
kbmhansungb <kbmhansungb@gmail.com>
2025-03-14 12:09:34 +0900

    비율 테스트트

--- js/contact-loader.js
+++ js/contact-loader.js
@@ -9,7 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
       })
       .then(html => {
         document.getElementById('contact-placeholder').innerHTML = html;
+        initializeContact();
       })
       .catch(error => console.error('에러 발생:', error));
   });
-  
+
+  function initializeContact()
+  {
+      // config.json에서 카카오톡 채널 URL을 가져와서 버튼의 href에 할당
+      fetch('/data/config.json')
+      .then(response => response.json())
+      .then(config => {
+        const kakaoLink = document.getElementById('kakaoLink');
+        kakaoLink.href = config.kakaoChannelUrl;
+      })
+      .catch(error => console.error('설정 파일 로드 실패:', error));
+  }
+
