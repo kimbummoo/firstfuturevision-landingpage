fb29c71ad0e66bc297ce286c1cd11edc6c898b2e
kbmhansungb <kbmhansungb@gmail.com>
2025-03-15 14:17:52 +0900

    링크 직접 입력

--- js/contact-loader.js
+++ js/contact-loader.js
@@ -16,13 +16,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function initializeContact()
   {
-      // config.json에서 카카오톡 채널 URL을 가져와서 버튼의 href에 할당
-      fetch('/firstfuturevision-landingpage/data/config.json')
-      .then(response => response.json())
-      .then(config => {
-        const kakaoLink = document.getElementById('kakaoLink');
-        kakaoLink.href = config.kakaoChannelUrl;
-      })
-      .catch(error => console.error('설정 파일 로드 실패:', error));
+      // // config.json에서 카카오톡 채널 URL을 가져와서 버튼의 href에 할당
+      // fetch('/firstfuturevision-landingpage/data/config.json')
+      // .then(response => response.json())
+      // .then(config => {
+      //   const kakaoLink = document.getElementById('kakaoLink');
+      //   kakaoLink.href = config.kakaoChannelUrl;
+      // })
+      // .catch(error => console.error('설정 파일 로드 실패:', error));
   }
 
