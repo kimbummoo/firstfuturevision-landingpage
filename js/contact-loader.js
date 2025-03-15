1828f68a1a47ff6852444f0dd826062530fda818
kbmhansungb <kbmhansungb@gmail.com>
2025-03-15 14:14:03 +0900

    링크 수정

--- js/contact-loader.js
+++ js/contact-loader.js
@@ -17,12 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
   function initializeContact()
   {
       // config.json에서 카카오톡 채널 URL을 가져와서 버튼의 href에 할당
-      fetch('/data/config.json')
+      fetch('/firstfuturevision-landingpage/data/config.json')
       .then(response => response.json())
       .then(config => {
         const kakaoLink = document.getElementById('kakaoLink');
-        // kakaoLink.href = config.kakaoChannelUrl;
-        kakaoLink.href = "http://pf.kakao.com/_iFQnn/chat";
+        kakaoLink.href = config.kakaoChannelUrl;
       })
       .catch(error => console.error('설정 파일 로드 실패:', error));
   }
